4979747bbeacaaa3c9f67326d90b2d91042bc4eb
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-09 17:53:18 +0100

    printing logic on the page

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
 const userChoiceElement = document.getElementById('pari-o-dispari');
 const userNumberElement = document.getElementById('user-number');
 const form = document.querySelector('form');
-
+const resultElement = document.getElementById('result')
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -15,19 +15,24 @@ form.addEventListener('submit', function(e) {
     const userChoice = userChoiceElement.value;
     const userNumber = parseInt(userNumberElement.value);
     const CPUNumber = getRandomInteger(); 
-    let message = 'La somma è dispari!'
+    let oddOrEvenMessage = 'dispari';
+    let message = ''
 
-    // logSomething(userChoice);
-    // logSomething(userNumber);
-    // logSomething(CPUNumber);
+    logSomething(userChoice);
+    logSomething(userNumber);
+    logSomething(CPUNumber);
 
-    const sum = isSumEven(userNumber, CPUNumber);
+    const sum = userNumber + CPUNumber;
+    const isEven = isSumEven(userNumber, CPUNumber);
 
-    if (sum) {
-        message = 'la somma è pari'
+    if (isEven) {
+        oddOrEvenMessage = 'pari'
     }
 
-    logSomething(message)
+    message = `Il tuo numero è <strong>${userNumber}</strong>; il numero della CPU è <strong>${CPUNumber}</strong>. <br>
+    La loro somma è <strong>${sum}</strong> ed è <strong>${oddOrEvenMessage}</strong>!`
+
+    resultElement.innerHTML = message;
     
     
 })
