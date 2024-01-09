f3858a15eb4193839887d5d61e2f2e99b23ddc7c
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-09 16:44:13 +0100

    finished palindroma

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,33 @@
-// console.log('functions ok');
+
+/** Console log of anything
+ * 
+ * @param {*} something value that can be anything
+ */
+
+function logSomething(something) {
+    console.log(something);
+}
 
 
+/** It checks if a word, when reversed, is the same as not reversed
+ * 
+ * @param {*} word a word
+ * @returns {boolean}
+ */
 
 function isPalindrome (word) {
     let isPalindrome = false;
     let reverseWord = ''
 
     for (let i = word.length - 1; i >= 0; i--) {
-        console.log(word[i]);
         reverseWord += word[i];
-        
     }
     
-    console.log(reverseWord);
-    
     if (reverseWord === word) {
-        console.log('è uguale');
         isPalindrome = true;
     } 
 
     return isPalindrome;
 }
-
-const word = 'osso';
-
-const result = isPalindrome(word);
-console.log(result);
-
 
 
--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
-// console.log('main ok')
-
 // * Palindroma
+
+const form = document.querySelector('form');
+const inputElement = document.querySelector('input');
+const resultElement = document.getElementById('result');
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    resultElement.innerText = '';
+    message = 'La tua parola non è palindrama!';
+
+    const wordFromUser = inputElement.value;
+    logSomething(wordFromUser);
+    
+    //! Validazione 
+
+    if (!wordFromUser) {
+        message = 'Devi inserire una parola!';    
+    }
+
+    if (wordFromUser && isPalindrome(wordFromUser)) {
+        message = 'La tua parola è palindrama!'
+    }
+
+    
+    resultElement.innerText = message;
+
+});
+
+
 
