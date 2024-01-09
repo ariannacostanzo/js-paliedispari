719209c7047600c98381354838899a1afa284966
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-09 18:23:58 +0100

    added images and style

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ const userNumberElement = document.getElementById('user-number');
 const form = document.querySelector('form');
 const resultElement = document.getElementById('result');
 const errorElement = document.getElementById('error-message');
+const youWin = document.getElementById('winner');
+const youLose = document.getElementById('lose');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -24,16 +26,25 @@ form.addEventListener('submit', function(e) {
     logSomething(userNumber);
     logSomething(CPUNumber);
 
-    // // ! Validazione
+    // ! Validazione
 
-    // if (isNaN(userNumber) || userNumber > 5 || userNumber < 5) {
-    //     errorMessage = 'Devi inserire un numero tra 1 e 5!'
-    //     errorElement.innerText = errorMessage;
-    //     return;
+    if (userChoice === 'Scegli') {
+        errorMessage = 'Devi scegliere tra pari e dispari!';
+        errorElement.innerText = errorMessage;
+        resultElement.innerText = '';
+        return;
+
+    } else if (isNaN(userNumber) || userNumber > 5 || userNumber < 0) {
+        errorMessage = 'Devi inserire un numero tra 1 e 5!'
+        errorElement.innerText = errorMessage;
+        resultElement.innerText = '';
+        return;
         
-    // } 
+    } 
 
 
+    errorElement.innerText = ''
+    
     const sum = userNumber + CPUNumber;
     const isEven = isSumEven(userNumber, CPUNumber);
 
@@ -46,8 +57,12 @@ form.addEventListener('submit', function(e) {
 
     if (userChoice === oddOrEvenMessage) {
         message += `<br>Hai scelto <strong>${userChoice}</strong>, dunque hai <strong>vinto</strong>!`
+        youLose.classList.add('d-none');
+        youWin.classList.remove('d-none');
     }   else {
         message += `<br>Hai scelto <strong>${userChoice}</strong>, dunque hai <strong>perso</strong>!`
+        youWin.classList.add('d-none');
+        youLose.classList.remove('d-none');
     }
 
     resultElement.innerHTML = message;
